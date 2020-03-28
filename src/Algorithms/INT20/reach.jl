a94f11316f507a014dd27962844060af7eacc805
mforets <mforets@gmail.com>
2020-03-28 10:06:01 -0300

    add INT files

---
+++ src/Algorithms/INT20/reach.jl
@@ -0,0 +1,24 @@
+# ================
+# Homogeneous case
+# ================
+
+function reach_homog_INT20!(F::Vector{ReachSet{N, Interval{N, IA.Interval{N}}}},
+                            Ω0::Interval{N, IA.Interval{N}},
+                            Φ::N,
+                            NSTEPS::Integer,
+                            δ::Float64,
+                            X::Universe) where {N}
+
+    # initial reach set
+    Δt = zero(N) .. δ
+    @inbounds F[1] = ReachSet(Ω0, Δt)
+
+    k = 2
+    @inbounds while k <= NSTEPS
+        Rₖ = Interval(Φ * set(F[k-1]).dat)
+        Δt += δ
+        F[k] = ReachSet(Rₖ, Δt)
+        k += 1
+    end
+    return F
+end
