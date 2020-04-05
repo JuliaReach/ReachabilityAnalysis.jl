9d7c1722c54cb6c86a9c76f39bdc0618e5d4db7d
mforets <mforets@gmail.com>
2020-04-05 16:43:29 -0300

    add BOX files

---
+++ src/Algorithms/BOX/BOX.jl
@@ -0,0 +1,36 @@
+"""
+    BOX{N, AM} <: AbstractContinuousPost
+
+Implementation of an integrator for linear systems usng box approximations.
+
+## Fields
+
+- `δ`            -- step-size of the discretization
+- `approx_model` -- (optional) approximation model for the discretization of the
+                    continuous ODE; see the `Notes` below for available options
+
+## Notes
+
+The type fields are:
+
+- `N`  -- number type of the step-size
+- `AM` -- approximation model
+
+The default approximation model used in this algorithm is:
+
+```julia
+Forward(sih=:concrete, exp=:base, phi2=:base, setops=:lazy)
+```
+This algorithm overapproximates the discrete sequence ``X_{k+1} = ΦX_k ⊕ V_k``
+by computing a tight hyperrectangular over-approximation of ``X_{k+1}`` at each
+step ``k ∈ \\mathbb{N}``.
+"""
+@with_kw struct BOX{N, AM} <: AbstractContinuousPost
+    δ::N
+    approx_model::AM=Forward(sih=:concrete, exp=:base, phi2=:base, setops=:lazy)
+end
+
+step_size(alg::BOX) = alg.δ
+
+include("post.jl")
+include("reach.jl")
---
+++ src/Algorithms/BOX/post.jl
@@ -0,0 +1,50 @@
+function post(alg::BOX, ivp::IVP{<:AbstractContinuousSystem}, tspan; kwargs...)
+
+    @unpack δ, approx_model = alg
+
+    if haskey(kwargs, :NSTEPS)
+        NSTEPS = kwargs[:NSTEPS]
+        T = NSTEPS * δ
+    else
+        # get time horizon from the time span imposing that it is of the form (0, T)
+        T = _get_T(tspan, check_zero=true, check_positive=true)
+        NSTEPS = ceil(Int, T / δ)
+    end
+
+    # normalize system to canonical form
+    ivp_norm = _normalize(ivp)
+
+    # discretize system
+    ivp_discr = discretize(ivp_norm, δ, approx_model)
+    Φ = state_matrix(ivp_discr)
+    Ω0 = initial_state(ivp_discr)
+    X = stateset(ivp_discr)
+
+    # true <=> there is no input, i.e. the system is of the form x' = Ax, x ∈ X
+    got_homogeneous = !hasinput(ivp_discr)
+
+    # this algorithm requires Ω0 to be hyperrectangle
+    Ω0 = overapproximate(Ω0, Hyperrectangle)
+
+    # reconvert the set of initial states and state matrix, if needed
+    static = haskey(kwargs, :static) ? kwargs[:static] : false
+    Ω0 = _reconvert(Ω0, Val(static))
+    Φ = _reconvert(Φ, Val(static))
+
+    # preallocate output flowpipe
+    N = eltype(Ω0)
+    HT = typeof(Ω0)
+    F = Vector{ReachSet{N, HT}}(undef, NSTEPS)
+
+    if got_homogeneous
+        reach_homog_BOX!(F, Ω0, Φ, NSTEPS, δ, X)
+    else
+        error("not implemented yet")
+        U = inputset(ivp_discr)
+        @assert isa(U, LazySet)
+        U = overapproximate(U, Hyperrectangle)
+        reach_inhomog_BOX!(F, Ω0, Φ, NSTEPS, δ, X, U)
+    end
+
+    return Flowpipe(F)
+end
