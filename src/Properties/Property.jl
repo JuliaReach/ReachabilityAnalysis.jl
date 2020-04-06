94a1c5ccf9182c2f23e2126faf28bee6f22ad69e
mforets <mforets@gmail.com>
2020-04-05 21:11:18 -0300

    remove some files'

--- src/Properties/Property.jl
+++
@@ -1,8 +0,0 @@
-"""
-Abstract supertype of properties that can be checked.
-
-Every concrete subtype should provide the following functions:
-  - `dim(𝑃::Property)::Int`
-  - `check(𝑃::Property, X::LazySet; witness::Bool=false)`
-"""
-abstract type Property end
