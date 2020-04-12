47899a30e11f6656517a4ce30ca995601645047b
mforets <mforets@gmail.com>
2020-04-12 02:51:03 -0300

    add invariant to model

--- test/models/linear/exponential1D.jl
+++ test/models/linear/exponential1D.jl
@@ -1,5 +1,5 @@
-function exponential_1d()
-    s = @system(x' = -x)
+function exponential_1d(; invariant=Universe(1))
+    s = @system(x' = -x, x ∈ invariant)
     x0 = Interval(0.4, 0.5)
     prob = InitialValueProblem(s, x0)
     tspan = (0.0, 1.0)
