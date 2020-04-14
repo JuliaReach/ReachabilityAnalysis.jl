861b11b88c3c1e1f3bcb6074d2fc2ef209f4eeb5
Daniel F Caporale <dfcaporale@gmail.com>
2020-04-14 14:35:39 -0300

    ABS07 new test linear ODE

--- test/algorithms/ASB07.jl
+++ test/algorithms/ASB07.jl
@@ -1,8 +1,6 @@
 @testset "ASB07 algorithm" begin
-
     # example 1 from
     # "Reachability analysis of linear systems with uncertain parameters and inputs"
-    # (Adapted from Reachability.jl)
 
     # initial set
     X0 = BallInf([1.0, 1.0], 0.1)
@@ -12,31 +10,30 @@
                          4.0 ± 0.05 -1.0 ± 0.05])
     P_lin = @ivp(x' = Ax, x(0) ∈ X0)
 
-    # TODO affine ODE: x' = Ax + Bu
-    # B = IntervalMatrix(hcat([1.0 ± 0.01; 1.0 ± 0.0]))
-    # u = ConstantInput(Interval(-0.05, 0.05))
-    # P_aff = @ivp(x' = Ax + Bu, x(0) ∈ X0)
+    sol1 = solve(P_lin, tspan=(0.0, 1.0), alg=ASB07(δ=0.04));
+    @test dim(sol1)==2
+    @test isa(sol1.alg, ASB07)
+    @test sol1.alg.δ==0.04
+    @test sol1.alg.max_order==10
+    @test typeof(sol1.alg.approx_model)==CorrectionHull
+    @test sol1.alg.approx_model.order==10
+    @test setrep(sol1) <: Zonotope
+    @test setrep(sol1)==Zonotope{Float64,Array{Float64,1},Array{Float64,2}}
 
-    f1(P) = solve(P, tspan=(0.0, 1.0), alg=ASB07(δ=0.04));
-
-    f2(P) = solve(P, tspan=(0.0, 5.0), alg=ASB07(δ=0.04, max_order=10),
-                  order_discretization = 4);
-
-    f3(P) = solve(P, tspan=(0.0, 1.0), alg=ASB07(δ=0.04),
-                  order_discretization = 4, vars =[1, 2],
-                  partition = [[1], [2]]);
-
-    f4(P) = solve(P, tspan=(0.0, 5.0), alg=ASB07(δ=0.04, max_order=10),
-                  order_discretization = 4, vars =[1, 2],
-                  partition = [[1], [2]]);
-
-    for P_t in [P_lin] #[P_lin, P_aff]
-        # default options
-        for funC in [f1, f2, f3, f4]
-            s = funC(P_t)
-            @test typeof(s.alg) <: ASB07
-            @test setrep(f1(P_lin)) <: Zonotope
-        end
-    end
+    sol2 = solve(P_lin, tspan=(0.0, 5.0), alg=ASB07(δ=0.01, max_order=7,
+                 approx_model=CorrectionHull(order=9, exp=:base)));
+    @test dim(sol2)==2
+    @test isa(sol2.alg, ASB07)
+    @test sol2.alg.δ==0.01
+    @test sol2.alg.max_order==7
+    @test typeof(sol2.alg.approx_model)==CorrectionHull
+    @test sol2.alg.approx_model.order==9
+    @test setrep(sol2) <: Zonotope
+    @test setrep(sol2)==Zonotope{Float64,Array{Float64,1},Array{Float64,2}}
 
 end
+
+# TODO affine ODE: x' = Ax + Bu
+# B = IntervalMatrix(hcat([1.0 ± 0.01; 1.0 ± 0.0]))
+# u = ConstantInput(Interval(-0.05, 0.05))
+# P_aff = @ivp(x' = Ax + Bu, x(0) ∈ X0)
