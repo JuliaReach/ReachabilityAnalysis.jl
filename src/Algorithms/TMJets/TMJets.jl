7b1c46f739564e4450a7b42576cdc81681a5d0cc
mforets <mforets@gmail.com>
2020-06-02 17:03:51 -0300

    let common options for TMJets be passed outside the struct

--- src/Algorithms/TMJets/TMJets.jl
+++ src/Algorithms/TMJets/TMJets.jl
@@ -26,14 +26,19 @@ then performs the disjointness check with that zonotope and the invariant.
 For other options, see the documentation of `AbstractDisjointnessMethod`.
 """
 @with_kw struct TMJets{N, DM<:AbstractDisjointnessMethod} <: AbstractContinuousPost
-    max_steps::Int=2000
-    abs_tol::N=1e-10
-    orderT::Int=8
-    orderQ::Int=2
+    max_steps::Int=DEFAULT_MAX_STEPS_TMJETS
+    abs_tol::N=DEFAULT_ABS_TOL_TMJETS
+    orderT::Int=DEFAULT_ORDER_T_TMJETS
+    orderQ::Int=DEFAULT_ORDER_Q_TMJETS
     disjointness::DM=ZonotopeEnclosure()
     adaptive::Bool=true
     min_abs_tol::N=1e-29
 end
+
+const DEFAULT_MAX_STEPS_TMJETS = 2000
+const DEFAULT_ABS_TOL_TMJETS = 1e-10
+const DEFAULT_ORDER_T_TMJETS = 8
+const DEFAULT_ORDER_Q_TMJETS = 2
 
 using TaylorModels: TaylorModelN
 using TaylorModels: fp_rpa, remainder
