5e3ef5010213a3896d6d24f1c9aa9d22dd685615
mforets <mforets@gmail.com>
2020-06-16 09:58:56 -0300

    refactor test models

---
+++ test/models/linear/interval2D.jl
@@ -0,0 +1,21 @@
+# ============================================================================
+# Example 1 from *Reachability analysis of linear systems with uncertain
+# parameters and inputs*. TODO: Agregar referencia completa.
+# ============================================================================
+
+# initial set
+X0 = BallInf([1.0, 1.0], 0.1)
+
+# linear ODE: x' = Ax
+A = IntervalMatrix([-1.0 ± 0.05 -4.0 ± 0.05;
+                    4.0 ± 0.05 -1.0 ± 0.05])
+
+# IVP(LCS(A), X0) TODO: remove
+interval2D_linear = @ivp x' = Ax, x(0) ∈ X0
+
+# affine ODE: x' = Ax + Bu
+B = IntervalMatrix(hcat([1.0 ± 0.0; 1.0 ± 0.0])) # why hcat?
+U = Interval(-0.05, 0.05)
+
+# IVP(CLCCS(A, B, nothing, U), X0) TODO: remove
+interval2D_affine = @ivp x' = Ax + Bu, x(0) ∈ X0, u ∈ U
