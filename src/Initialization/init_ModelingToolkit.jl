2139610804ae66b8d926d118a5db74ce2457aa60
mforets <mforets@gmail.com>
2020-07-24 17:34:18 -0300

    cleanup MT code

--- src/Initialization/init_ModelingToolkit.jl
+++ src/Initialization/init_ModelingToolkit.jl
@@ -1,11 +1,7 @@
 eval(quote
     using .ModelingToolkit: get_variables,
-                            gradient,
-                            simplify,
-                            to_symbolic,
                             Operation
 
 end)
 
-eval(load_modeling_toolkit_hyperplane())
-eval(load_modeling_toolkit_halfspace())
+eval(load_modeling_toolkit_polyhedron())
