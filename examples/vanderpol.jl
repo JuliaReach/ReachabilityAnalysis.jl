97c433e7a86bc9e28ac45c4e7643b92065a378e5
SebastianGuadalupe <sebastianguadalupe00@gmail.com>
2020-08-06 14:56:27 -0300

    update vanderpol

--- examples/vanderpol.jl
+++ examples/vanderpol.jl
@@ -81,17 +81,50 @@ plot!(x -> 2.75, color=:red, lab="y = 2.75", style=:dash, legend=:bottomright) #
 #
 # We can use the reachability result to examine the limit cycle of the system. In
 # other words, we can see that the flowpipe re-enters from where it started after
-# giving one loop.
+# giving one loop. To examine this we can intersect a line somewhat perpendicular
+# to the trajectory, that will allow us to get a cross-section of the sets
 
 plot(solz, vars=(1, 2), lw=0.2, xlims=(0.0, 2.5), ylims=(1.6, 2.8), xlab="x", ylab="y") #!jl
 plot!(X0, color=:orange, lab="X0") #!jl
 plot!(solz[1:5], vars=(1, 2), color=:green, lw=1.0, alpha=0.5, lab="F[1:5]") #!jl
 plot!(solz[200], vars=(1, 2), color=:red, lw=1.0, alpha=0.6, lab="F[200]") #!jl
+plot!(LineSegment([1, 2.], [2., 2.5]), lw=2.0) #!jl
 
-# It is seen that the reach-set corresponding to the time-span
+# Then we can define a function to get the cross section of the flowpipe, the
+# function needs the flowpipe, a line segment that cuts the flowpipe and the
+# indices of the subsets to cut
 
-tspan(solz[200])  #!jl
+function cross_section(line::LineSegment, RS::ReachSolution, idx) #!jl
+    i = reduce(convex_hull, map(x -> intersection(line, x), set.(RS[idx]))) #!jl
+    vl = vertices_list(i) #!jl
+    return LineSegment(vl[1], vl[2]) #!jl
+end #!jl
 
-# is included in the set union ``F[1] \cup \cdots \cup F[5]`` of previously
-# computed reach-sets. This in fact constitutes a proof that the system has a limit cycle,
-# because all future trajectories starting from `solz[200]` are already covered by the flowpipe.
+# Then we can get the cross section of the first five sets and the last set,
+# calling them `i1` and `i2` respectively.
+
+i1 = cross_section(line, solz, 1:5) #!jl
+i2 = cross_section(line, solz, [200]) #!jl
+
+# We can also calculate the length of each cross section, remember that the
+# system is 2D, so the cross section will be a line segment.
+
+l1 = norm(i1.q - i1.p) #!jl
+l2 = norm(i2.q - i2.p); #!jl
+@show l1 #!jl
+@show l2; #!jl
+
+#-------
+
+plot(i1, lw=3.0, alpha=1.0, label="First subsets", legend=:bottomright) #!jl
+plot!(i2, lw=5.0, alpha=1.0, label="Last subset") #!jl
+
+#-------
+
+i2 ⊆ i1
+
+# We can see, the cross section of the las subset is a subset of the first few
+# sets, thus, the cycle will continue, presumably getting smaller each revolution.
+
+# This in fact constitutes a proof that the system has a limit cycle, because all
+# future trajectories starting from `solz[200]` are already covered by the flowpipe.
