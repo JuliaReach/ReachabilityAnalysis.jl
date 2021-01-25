ed0d6bd2bb52eb6aa3aceaf82f44c7d0e0f59d10
Marcelo Forets <mforets@gmail.com>
2021-01-25 14:25:43 -0300

    Update docs/generate.jl

--- docs/generate.jl
+++ docs/generate.jl
@@ -24,7 +24,7 @@ MODELS = ["Brusselator/Brusselator.jl",
           #"LotkaVolterraTangential/LotkaVolterraTangential.jl",
           "OpAmp/OpAmp.jl",
           "SquareWaveOscillator/SquareWaveOscillator.jl",
-          #"Platoon/Platoon.jl", # FIXME requires ModelingToolkit v5.0
+          "Platoon/Platoon.jl",
           "ProductionDestruction/ProductionDestruction.jl",
           "Quadrotor/Quadrotor.jl",
           "SEIR/SEIR.jl",
