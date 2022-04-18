66a2cb54612e55e96c7e92543cef4b2d433e71fd
Marcelo Forets <mforets@gmail.com>
2022-04-18 11:55:39 -0300

    Update TaylorModelReachSet.jl

--- src/ReachSets/TaylorModelReachSet.jl
+++ src/ReachSets/TaylorModelReachSet.jl
@@ -13,7 +13,8 @@ Reach-set representation consisting of a vector of taylor models in one variable
 
 ### Notes
 
-The parameter `N` refers to the numerical type of the representation.
+The parameters `N` and `S` refer to the numerical type of the representation (used for the Taylor model
+in time and Taylor series polynomials respectively).
 The space variables are assumed to be normalized to the interval `[-1, 1]`.
 It is assumed that the time domain is the same for all components.
 """
