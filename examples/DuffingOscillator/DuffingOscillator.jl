ee2aa4cb7a66f99b91eb8d93e9c355e684fecb45
schillic <git@christianschilling.net>
2024-02-12 23:58:26 +0100

    revise DuffingOscillator

--- examples/DuffingOscillator/DuffingOscillator.jl
+++ examples/DuffingOscillator/DuffingOscillator.jl
@@ -1,13 +1,15 @@
-# # Duffing Oscillator
-#
+# # Duffing oscillator
+
 #md # !!! note "Overview"
-#md #     System type: nonlinear\
+#md #     System type: Polynomial continuous system\
 #md #     State dimension: 2\
 #md #     Application domain: Nonlinear physics
-#
-# ## Implementation
 
-using ReachabilityAnalysis, Plots
+# ## Model description
+
+using ReachabilityAnalysis  #!jl
+
+const ω = 1.2
 
 @taylorize function duffing!(du, u, p, t)
     local α = -1.0
@@ -16,26 +18,32 @@ using ReachabilityAnalysis, Plots
     local γ = 0.37
 
     x, v = u
+
     f = γ * cos(ω * t)
 
     du[1] = u[2]
-    return du[2] = -α * x - δ * v - β * x^3 + f
+    du[2] = -α * x - δ * v - β * x^3 + f
+    return du
 end
 
-ω = 1.2
-T = 2 * pi / ω
-X0 = Singleton([1.0, 0.0]) ⊕ BallInf(zeros(2), 0.1)
-prob = @ivp(x' = duffing!(x), x(0) ∈ X0, dim = 2);
+# ## Specification
 
-# ## Results
+X0 = Hyperrectangle([1.0, 0.0], [0.1, 0.1])
+prob = @ivp(x' = duffing!(x), x(0) ∈ X0, dim:2)
+
+T = 2 * pi / ω;
+
+# ## Analysis
 
 sol = solve(prob; tspan=(0.0, 20 * T), alg=TMJets21a());
 
-#-
+# ## Results
+
+using Plots  #!jl
+#!jl import DisplayAs  #hide
 
 fig = plot(sol; vars=(0, 1), xlab="t", ylab="x", lw=0.2, color=:blue)
 
-#!jl import DisplayAs  #hide
 #!jl DisplayAs.Text(DisplayAs.PNG(fig))  #hide
 
 #-
