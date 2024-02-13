ddfeef5fd1713f72f1351107a8663ac7b325e004
schillic <git@christianschilling.net>
2024-02-13 21:31:23 +0100

    revise LaubLoomis

--- examples/LaubLoomis/LaubLoomis.jl
+++ examples/LaubLoomis/LaubLoomis.jl
@@ -1,18 +1,19 @@
 # # Laub-Loomis
-#
+
 #md # !!! note "Overview"
-#md #     System type: polynomial with quadratic nonlinearities\
+#md #     System type: Polynomial continuous system\
 #md #     State dimension: 7\
-#md #     Application domain: Molecular Biology
-#
+#md #     Application domain: Molecular biology
+
 # ## Model description
-#
-# The Laub-Loomis model is presented in [^LL98] for studying a class of enzymatic activities.
-# The dynamics can be defined by the following ODE with 7 variables.
 
-using ReachabilityAnalysis, Plots
+# The Laub-Loomis model is presented in [^LL98] for studying a class of
+# enzymatic activities. The dynamics can be defined by the following
+# seven-dimensional system of differential equations.
 
-@taylorize function laubloomis!(dx, x, params, t)
+using ReachabilityAnalysis  #!jl
+
+@taylorize function laubloomis!(dx, x, p, t)
     dx[1] = 1.4 * x[3] - 0.9 * x[1]
     dx[2] = 2.5 * x[5] - 1.5 * x[2]
     dx[3] = 0.6 * x[7] - 0.8 * (x[2] * x[3])
@@ -25,112 +26,100 @@ end
 
 # The system is asymptotically stable and the equilibrium is the origin.
 
-# ## Specifications
+# ## Specification
+
+# The initial conditions are defined according to the ones used in [^TD13]. They
+# are boxes centered at ``x_1(0) = 1.2``, ``x_2(0) = 1.05``, ``x_3(0) = 1.5``,
+# ``x_4(0) = 2.4``, ``x_5(0) = 1``, ``x_6(0) = 0.1``, ``x_7 (0) = 0.45``. The
+# range of the box in the i-th dimension is defined by the interval
+# ``[x_i(0) − W, x_i(0) + W]``. We consider ``W = 0.01``, ``W = 0.05``, and
+# ``W = 0.1``. The specification for each scenario is given as follows:
 #
-# The specification for the analysis is given next.
-# The initial sets are defined according to the ones used in [^TD13].
-# They are boxes centered at
-# ``x_1(0) = 1.2``, ``x_2(0) = 1.05``, ``x_3(0) = 1.5``, ``x_4(0) = 2.4``,
-# ``x_5(0) = 1``, ``x_6(0) = 0.1``, ``x_7 (0) = 0.45``.
-# The range of the box in the i-th dimension is defined by the interval ``[x_i(0) − W, x_i(0) + W]``.
-# The width ``W`` of the initial set is vital to the difficulty of the reachability analysis job.
-# The larger the initial set the harder the reachability analysis.
+# - ``W = 0.01`` and ``W = 0.05``: The unsafe region is defined by ``x_4 ≥ 4.5``.
+# - ``W = 0.1``: The unsafe set is defined by ``x_4 ≥ 5``.
+#
+# The time horizon for all cases is ``[0, 20]``.
 
 function laubloomis(; W=0.01)
-    ## initial states
-    X0c = [1.2, 1.05, 1.5, 2.4, 1.0, 0.1, 0.45]
-    X0 = Hyperrectangle(X0c, fill(W, 7))
-
-    ## initial-value problem
+    X0 = Hyperrectangle([1.2, 1.05, 1.5, 2.4, 1.0, 0.1, 0.45], fill(W, 7))
     prob = @ivp(x' = laubloomis!(x), dim:7, x(0) ∈ X0)
-
     return prob
-end
+end;
 
-# We consider ``W = 0.01``, ``W = 0.05``, and ``W = 0.1``. The specification for each
-# scenario is the following:
-#
-# - ``W = 0.01`` and ``W = 0.05``: the unsafe region is defined by ``x_4 ≥ 4.5
-# - ``W = 0.1``: the unsafe set is defined by ``x_4 ≥ 5``.
-#
-# The time horizon for all cases is [0, 20].
+# ## Analysis
 
-# ## Results
+# The final widths of ``x_4`` along with the computation times are computed for
+# all three cases, as well as a figure in the ``(t, x_4)`` axes,
+# with ``t ∈ [0, 20]``, ``x_4 ∈ [1.5, 5]``, where the three plots are overlaid.
 
-# The final widths of ``x_4`` along with the computation times are provided for all three cases. A
-# figure is provided in the ``(t, x_4)`` axes, with ``t ∈ [0, 20]``, ``x_4 ∈ [1.5, 5]``,
-# where the three plots are overlaid.
-
-# Since the safety condition is only related to the variable ``x_4``, we present the
-# plots of projections of the overapproximations in the ``t - x_4`` plane such that
-# ``t`` is the time variable.
-
-# ### Case 1: Smaller initial states
-
-# In this case we consider thet the width of the initial states is ``W = 0.01``.
-
-prob = laubloomis(; W=0.01)
-alg = TMJets(; abstol=1e-11, orderT=7, orderQ=1, adaptive=true);
-
-sol_1 = solve(prob; T=20.0, alg=alg);
-
-sol_1z = overapproximate(sol_1, Zonotope);
-
-# Verifying that the specifications hold:
-
-## canonical direction along x₄
+# Canonical direction along x₄:
 const e4 = [0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0];
 
+# ### Case 1: Small initial set
+
+# In this case, we consider the width of the initial set ``W = 0.01``.
+
+prob = laubloomis(; W=0.01)
+alg = TMJets(; abstol=1e-11, orderT=7, orderQ=1, adaptive=true)
+
+sol_1 = solve(prob; T=20.0, alg=alg)
+sol_1z = overapproximate(sol_1, Zonotope);
+
+# We verify that the specification holds:
+
+@assert ρ(e4, sol_1z) < 4.5 "the property should be proven"
 ρ(e4, sol_1z)
-#-
-ρ(e4, sol_1z) < 4.5
 
-# To compute the width of the final box width of the final box we use support functions:
-@show ρ(e4, sol_1z[end]) + ρ(-e4, sol_1z[end])
+# To compute the width of the final box, we use the support function:
 
-# ### Case 2: Intermediate initial states
+ρ(e4, sol_1z[end]) + ρ(-e4, sol_1z[end])
 
-# In this case we consider the the width of the initial states is ``W = 0.05``.
+# ### Case 2: Intermediate initial set
+
+# In this case, we consider the width of the initial set ``W = 0.05``.
 
 prob = laubloomis(; W=0.05)
-alg = TMJets(; abstol=1e-12, orderT=7, orderQ=1, adaptive=false);
+alg = TMJets(; abstol=1e-12, orderT=7, orderQ=1, adaptive=false)
 
 sol_2 = solve(prob; T=20.0, alg=alg)
 sol_2z = overapproximate(sol_2, Zonotope);
 
-# Verifying that the specification holds:
+# We verify that the specification holds:
 
+@assert ρ(e4, sol_2z) < 4.5 "the property should be proven"
 ρ(e4, sol_2z)
-#-
-ρ(e4, sol_2z) < 4.5
 
-## width of final box
-@show ρ(e4, sol_2z[end]) + ρ(-e4, sol_2z[end])
+# Width of final box:
 
-# ### Case 3: Larger initial states
+ρ(e4, sol_2z[end]) + ρ(-e4, sol_2z[end])
 
-# In this case we consider that the width of the initial states is ``W = 0.1``.
+# ### Case 3: Large initial set
+
+# In this case, we consider the width of the initial set ``W = 0.1``.
 
 prob = laubloomis(; W=0.1)
-alg = TMJets(; abstol=1e-12, orderT=7, orderQ=1, adaptive=false);
+alg = TMJets(; abstol=1e-12, orderT=7, orderQ=1, adaptive=false)
 
 sol_3 = solve(prob; T=20.0, alg=alg)
 sol_3z = overapproximate(sol_3, Zonotope);
 
-# Verifying that the specification holds:
+# We verify that the specification holds:
+
+@assert ρ(e4, sol_3z) < 5.0 "the property should be proven"
 ρ(e4, sol_3z)
-#-
-ρ(e4, sol_3z) < 5.0
 
-## width of final box
-@show ρ(e4, sol_3z[end]) + ρ(-e4, sol_3z[end])
-#
-using Plots, Plots.PlotMeasures, LaTeXStrings #!jl
+# Width of final box:
 
-fig = plot()
-plot!(fig, sol_3z; vars=(0, 4), linecolor="green", color=:green, alpha=0.8)
-plot!(fig, sol_2z; vars=(0, 4), linecolor="blue", color=:blue, alpha=0.8)
-plot!(fig, sol_1z; vars=(0, 4), linecolor="yellow", color=:yellow, alpha=0.8,
+ρ(e4, sol_3z[end]) + ρ(-e4, sol_3z[end])
+
+# ## Results
+
+using Plots, Plots.PlotMeasures, LaTeXStrings  #!jl
+#!jl import DisplayAs  #hide
+
+fig = plot(sol_3z; vars=(0, 4), linecolor="green", color=:green, alpha=0.8, lab="W = 0.1")
+plot!(fig, sol_2z; vars=(0, 4), linecolor="blue", color=:blue, alpha=0.8, lab="W = 0.05")
+plot!(fig, sol_1z; vars=(0, 4), linecolor="yellow", color=:yellow, alpha=0.8, lab="W = 0.01",
       tickfont=font(10, "Times"), guidefontsize=15,
       xlab=L"t",
       ylab=L"x_4",
@@ -139,14 +128,17 @@ plot!(fig, sol_1z; vars=(0, 4), linecolor="yellow", color=:yellow, alpha=0.8,
       bottom_margin=6mm, left_margin=2mm, right_margin=4mm, top_margin=3mm,
       size=(600, 600))
 
-plot!(fig, x -> x, x -> 4.5, 0.0, 20.0; line=2, color="red", linestyle=:dash, legend=nothing)
-plot!(fig, x -> x, x -> 5.0, 0.0, 20.0; line=2, color="red", linestyle=:dash, legend=nothing)
+plot!(fig, x -> x, x -> 4.5, 0.0, 20.0; line=2, color="red", linestyle=:dash, lab="")
+plot!(fig, x -> x, x -> 5.0, 0.0, 20.0; line=2, color="red", linestyle=:dash, lab="")
 
-#!jl import DisplayAs  #hide
 #!jl DisplayAs.Text(DisplayAs.PNG(fig))  #hide
 
 # ## References
 
-# [^LL98]: M. T. Laub and W. F. Loomis. *A molecular network that produces spontaneous oscillations in excitable cells of dictyostelium.* Molecular Biology of the Cell, 9:3521–3532, 1998.
-
-# [^TD13]: R. Testylier and T. Dang. *NLTOOLBOX: A library for reachability computation of nonlinear dynamical systems.* In Proc. of ATVA’13, volume 8172 of LNCS, pages 469–473. Springer, 2013.
+# [^LL98]: M. T. Laub and W. F. Loomis. *A molecular network that produces
+#          spontaneous oscillations in excitable cells of dictyostelium*.
+#          Molecular Biology of the Cell, 9:3521–3532, 1998.
+#
+# [^TD13]: R. Testylier and T. Dang. *NLTOOLBOX: A library for reachability
+#          computation of nonlinear dynamical systems*. ATVA, volume 8172 of
+#          LNCS, pages 469–473. Springer, 2013.
