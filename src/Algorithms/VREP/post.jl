bf3ccd25e13c4f74dab7c602a31fe3303b2b0808
schillic <git@christianschilling.net>
2025-01-09 22:16:47 +0100

    add discrete post for VREP

--- src/Algorithms/VREP/post.jl
+++ src/Algorithms/VREP/post.jl
@@ -1,17 +1,11 @@
-# ===========================
-# Continuous post interface
-# ===========================
-
-# this algorithm uses polygons (two dimensions) or polytopes (any dimension) in vertex representation
-function post(alg::VREP{N}, ivp::IVP{<:AbstractContinuousSystem}, tspan;
-              Δt0::TimeInterval=zeroI, kwargs...) where {N}
-    @unpack δ, approx_model, static, dim = alg
+# continuous post
+function post(alg::VREP, ivp::IVP{<:AbstractContinuousSystem}, tspan;
+              Δt0::TimeInterval=zeroI, kwargs...)
+    δ = alg.δ
 
     NSTEPS = get(kwargs, :NSTEPS, compute_nsteps(δ, tspan))
 
     # normalize system to canonical form
-    # x' = Ax, x in X, or
-    # x' = Ax + u, x in X, u in U
     ivp_norm = _normalize(ivp)
 
     # homogenize the initial-value problem
@@ -20,20 +14,37 @@ function post(alg::VREP{N}, ivp::IVP{<:AbstractContinuousSystem}, tspan;
     end
 
     # discretize system
-    ivp_discr = discretize(ivp_norm, δ, approx_model)
-    Φ = state_matrix(ivp_discr)
+    ivp_discr = discretize(ivp_norm, δ, alg.approx_model)
+
+    return post(alg, ivp_discr, NSTEPS; Δt0=Δt0, kwargs...)
+end
+
+# discrete post
+function post(alg::VREP{N}, ivp::IVP{<:AbstractDiscreteSystem}, NSTEPS=nothing;
+              Δt0::TimeInterval=zeroI, kwargs...) where {N}
+    @unpack δ, approx_model, static, dim = alg
+
+    if isnothing(NSTEPS)
+        if haskey(kwargs, :NSTEPS)
+            NSTEPS = kwargs[:NSTEPS]
+        else
+            throw(ArgumentError("`NSTEPS` not specified"))
+        end
+    end
+
+    Φ = state_matrix(ivp)
 
     # the initial state should be expressed in v-rep
-    Ω0 = initial_state(ivp_discr)
+    Ω0 = initial_state(ivp)
     if !(Ω0 isa VPolygon || Ω0 isa VPolytope)
         n = size(Φ, 1)
         Ω0 = n == 2 ? convert(VPolygon, Ω0) : convert(VPolytope, Ω0)
     end
 
-    X = stateset(ivp_discr)
+    X = stateset(ivp)
 
     # true <=> there is no input, i.e. the system is of the form x' = Ax, x ∈ X
-    got_homogeneous = !hasinput(ivp_discr)
+    got_homogeneous = !hasinput(ivp)
 
     # reconvert the set of initial states and state matrix, if needed
     Ω0 = _reconvert(Ω0, static, dim)
