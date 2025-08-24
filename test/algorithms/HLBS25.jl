a722079f896c7728639a468ecf288c354de02bd5
alecarraro <a.carraro@student.tudelft.nl>
2025-08-24 20:20:13 +0200

    add tests

---
+++ test/algorithms/HLBS25.jl
@@ -0,0 +1,63 @@
+using ReachabilityAnalysis
+using LazySets
+
+@testset "HLBS25 algorithm: homogeneous case" begin
+    for N in (Float32, Float64)
+
+        # uncertain system matrix
+        AS = MatrixZonotope(N[-1 -5; 1 -1], [N[0 0; 0.1 0]], [6])
+
+        # initial set: box -> zonotope -> sparse polynomial zonotope
+        X0 = Zonotope(N[1.0, 1.0], [0.2 0; 0 0.2])
+        X0 = convert(SparsePolynomialZonotope, X0)
+
+        # problem definition
+        prob = @ivp(x' = A * x, x(0) ∈ X0, A ∈ AS)
+
+        δ = N(2π) / 200
+        T = 2 * N(π)
+
+        # recursive false
+        alg = HLBS25(
+            δ = δ,
+            approx_model = CorrectionHullMatrixZonotope(),
+            max_order = 5,
+            taylor_order = 5,
+            reduction_method = LazySets.GIR05(),
+            recursive = true,
+            tol = 1e-2,
+            norm = Inf,
+        )
+        sol1 = solve(prob, alg; T=T)
+
+        @test_broken X0 ⊆ set(sol[1]) #TODO add inclusion check for SPZ
+
+        @test isa(sol1.alg, HLBS25)
+        @test sol1.alg.δ == δ
+        @test sol1.alg.max_order == 4
+        @test sol1.alg.taylor_order == 10
+        @test typeof(sol1.alg.approx_model) == CorrectionHullMatrixZonotope
+        @test setrep(sol1) <: SparsePolynomialZonotope
+
+        # recursive true
+        alg2 = HLBS25(
+            δ = δ/2,
+            approx_model = CorrectionHullMatrixZonotope(),
+            max_order = 5,
+            taylor_order = 5,
+            reduction_method = LazySets.GIR05(),
+            recursive = true,
+            tol = 1e-2,
+            norm = Inf,
+        )
+        sol2 = solve(prob, alg2; T=T)
+
+        @test isa(sol2.alg, HLBS25)
+        @test sol2.alg.δ == δ/2
+        @test sol2.alg.max_order == 6
+        @test sol2.alg.taylor_order == 12
+        @test sol2.alg.tol == 1e-10
+        @test setrep(sol2) <: SparsePolynomialZonotope
+
+    end
+end
