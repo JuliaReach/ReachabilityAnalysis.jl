5a2e64d694b7725e9a76901a0401678d4ddc732c
schillic <git@christianschilling.net>
2025-12-14 22:37:35 +0100

    fix CARLIN algorithm in multiple dimensions

--- test/algorithms/CARLIN.jl
+++ test/algorithms/CARLIN.jl
@@ -19,3 +19,19 @@
     @test dim(sol) == 1
     @test ρ([1.0], sol(10.0)) < 0.01
 end
+
+@testset "CARLIN algorithm: SEIR model" begin
+    F1 = zeros(3, 3)
+    F1[1, 1] = -0.19
+    F1[2, 2] = -0.192308
+    F1[3, 2] = 0.192308
+    F1[3, 3] = -0.434783
+    F2 = zeros(3, 9)
+    F2[1, 3] = -1.3e-8
+    F2[2, 3] = 1.3e-8
+    X0 = Hyperrectangle([1.0, 1, 1], [0.1, 0.1, 0.1])
+    prob = @ivp(CanonicalQuadraticForm(F1, F2), x(0) ∈ X0)
+
+    sol = solve(prob; T=1.0, alg=CARLIN(; N=2, bloat=true))
+    @test dim(sol) == 3
+end
