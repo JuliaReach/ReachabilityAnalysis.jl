388ac11a0d2ea2309ff5b35e30398f25cae37344
schillic <git@christianschilling.net>
2026-01-09 00:13:52 +0100

    mark test for persistent tasks as broken

--- test/Aqua.jl
+++ test/Aqua.jl
@@ -5,5 +5,7 @@ import Aqua
     # note that the check `persistent_tasks` may take several minutes
     Aqua.test_all(ReachabilityAnalysis;
                   # the piracies should be resolved in the future
-                  piracies=(broken=true,))
+                  piracies=(broken=true,),
+                  # the test for persistent tasks started failing in 2026
+                  persistent_tasks=(broken=true,))
 end
