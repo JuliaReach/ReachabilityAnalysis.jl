7d54390191f67ea7c56d10a3582c89ef6868f09b
schillic <git@christianschilling.net>
2026-01-09 09:32:09 +0100

    deactivate Aqua.persistent_tasks test

--- test/Aqua.jl
+++ test/Aqua.jl
@@ -5,5 +5,7 @@ import Aqua
     # note that the check `persistent_tasks` may take several minutes
     Aqua.test_all(ReachabilityAnalysis;
                   # the piracies should be resolved in the future
-                  piracies=(broken=true,))
+                  piracies=(broken=true,),
+                  # persistent tasks started failing occasionally on CI runs in January 2026
+                  persistent_tasks=false)
 end
