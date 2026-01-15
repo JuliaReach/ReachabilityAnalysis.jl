5877df2bef2367ff19a520d1ee310d854aa8b5b4
schillic <git@christianschilling.net>
2026-01-15 22:19:13 +0100

    add BFFPSV18Module

---
+++ src/Algorithms/BFFPSV18/BFFPSV18Module.jl
@@ -0,0 +1,26 @@
+module BFFPSV18Module
+
+using ..ReachabilityAnalysis: AbstractContinuousPost, Flowpipe, SparseReachSet,
+                              TimeInterval, hasinput, zeroI, _isdisjoint
+using ..ForwardModule: Forward
+
+using IntervalArithmetic: (..)
+using LazySets: CartesianProductArray, Hyperrectangle, Interval, LazySet,
+                LinearMap, MinkowskiSumArray, Projection, Universe, decompose,
+                overapproximate, ⊕
+using LinearAlgebra: mul!
+using MathematicalSystems: AbstractDiscreteSystem, IVP, initial_state, inputset,
+                           state_matrix, stateset
+using Parameters: @unpack
+using SparseArrays: SparseMatrixCSC, sparse
+
+import ..ReachabilityAnalysis: post, numtype, setrep, step_size, rsetrep
+
+include("BFFPSV18.jl")
+include("post.jl")
+include("reach_homog.jl")
+include("reach_inhomog.jl")
+
+export BFFPSV18
+
+end  # module
