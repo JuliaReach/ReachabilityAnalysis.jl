b26f678eae21611011c0ed9adc310b55d1134977
schillic <git@christianschilling.net>
2026-01-17 19:20:20 +0100

    add INTModule

---
+++ src/Algorithms/INT/INTModule.jl
@@ -0,0 +1,23 @@
+module INTModule
+
+using ..ReachabilityAnalysis: AbstractContinuousPost, Flowpipe, ReachSet,
+                              TimeInterval, hasinput, zeroI,
+                              _is_intersection_empty
+using ..ForwardModule: Forward
+
+using IntervalArithmetic: (..)
+using LazySets: Interval, LazySet, Universe, overapproximate, set
+using MathematicalSystems: AbstractDiscreteSystem, IVP, initial_state, inputset,
+                           state_matrix, statedim, stateset
+using Parameters: @unpack
+
+import ..ReachabilityAnalysis: post, numtype, setrep, step_size, rsetrep
+
+include("INT.jl")
+include("post.jl")
+include("reach_homog.jl")
+include("reach_inhomog.jl")
+
+export INT
+
+end  # module
