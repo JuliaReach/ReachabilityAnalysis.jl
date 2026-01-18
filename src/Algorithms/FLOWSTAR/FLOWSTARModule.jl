d63c4fb3360adbaa8d60d15d1dd193fc11999941
schillic <git@christianschilling.net>
2026-01-18 11:43:27 +0100

    add FLOWSTARModule

---
+++ src/Algorithms/FLOWSTAR/FLOWSTARModule.jl
@@ -0,0 +1,23 @@
+module FLOWSTARModule
+
+using ..ReachabilityAnalysis: AbstractContinuousPost, Flowpipe, TimeInterval,
+                              TaylorModelReachSet, domain, tend, tstart, zeroI
+
+using IntervalArithmetic: IntervalBox, sup
+import IntervalArithmetic as IA
+using LazySets: Hyperrectangle, overapproximate
+using MathematicalSystems: AbstractContinuousSystem, BlackBoxContinuousSystem,
+                           IVP, initial_state, statedim, system
+using Parameters: @unpack
+using ReachabilityBase.Require: @required
+using Requires: @require
+
+import ..ReachabilityAnalysis: post, numtype, rsetrep
+
+include("FLOWSTAR.jl")
+
+include("init.jl")
+
+export FLOWSTAR
+
+end  # module
