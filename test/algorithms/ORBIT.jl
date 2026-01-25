843146c7c515ecb635a1430b21c23d1f20df3aca
schillic <git@christianschilling.net>
2026-01-25 09:21:35 +0100

    add tests for 'ORBIT' algorithm

--- test/algorithms/ORBIT.jl
+++ test/algorithms/ORBIT.jl
@@ -1,7 +1,22 @@
-using LazySets: _isapprox
+using ReachabilityAnalysis, Test
+import ReachabilityAnalysis as RA
+using ReachabilityAnalysis.ReachabilityBase.Comparison: _isapprox
+
+@testset "ORBIT struct" begin
+    # full constructor (requires to manually define the type parameter for the set representation)
+    alg = ORBIT{Float64,Int,Int}(0.01, 0)
+
+    # constructor with default values
+    alg = ORBIT(; δ=0.01)
+
+    # struct getters
+    @test RA.step_size(alg) == 0.01
+    @test RA.numtype(alg) == Float64
+    @test setrep(alg) == Singleton{Float64,Vector{Float64}}
+    @test rsetrep(alg) == ReachSet{Float64,setrep(alg)}
+end
 
 @testset "ORBIT algorithm" begin
-
     # --------------------------------------------------------------------------
     # harmonic oscillator x'' = -x, s.t. x(0) = 1, x'(0) = 0
     # --------------------------------------------------------------------------
@@ -23,6 +38,41 @@ using LazySets: _isapprox
 
     # test time span sequence
     @test all(_isapprox(tspan(sol[i]), interval(δ * (i - 1))) for i in eachindex(sol))
+
+    # `homogenize` option
+    @test_broken solve(prob; tspan=(0.0, 3δ), alg=ORBIT(; δ=δ), homogenize=true) isa
+                 RA.ReachSolution  # TODO fix this
+
+    # start from a ZeroSet
+    prob0 = IVP(prob.s, ZeroSet(2))
+    @test_broken solve(prob0; tspan=(0.0, 3δ), alg=ORBIT(; δ=δ)) isa RA.ReachSolution  # TODO fix this
+
+    # --------------------------------------------------------------------------
+    # inhomogeneous problem
+    # --------------------------------------------------------------------------
+    A = prob.s.A
+    X0 = prob.x0
+    B = [2.0 0; 0 1]
+    X = Universe(2)
+
+    # AbstractInput
+    U = ConstantInput(Singleton([0.1, 0.1]))
+    prob = @ivp(x' = A * x + B * u, x(0) ∈ X0, u ∈ U, x ∈ X)
+    sol = solve(prob; tspan=(0.0, 3δ), alg=ORBIT(; δ=δ)) isa RA.ReachSolution
+
+    # start from a ZeroSet and U is a Singleton
+    prob = @ivp(x' = A * x + u, x(0) ∈ ZeroSet(2), u ∈ U, x ∈ X)
+    sol = solve(prob; tspan=(0.0, 3δ), alg=ORBIT(; δ=δ))
+
+    # nondeterministic inputs
+    U = BallInf([0.1, 0.1], 0.0)
+    prob = @ivp(x' = A * x + B * u, x(0) ∈ X0, u ∈ U, x ∈ X)
+    @test_broken solve(prob; tspan=(0.0, 3δ), alg=ORBIT(; δ=δ)) isa RA.ReachSolution  # TODO fix this
+
+    # invariant
+    X = BallInf(zeros(2), 100.0)
+    prob = @ivp(x' = A * x + B * u, x(0) ∈ X0, u ∈ U, x ∈ X)
+    @test_broken solve(prob; tspan=(0.0, 3δ), alg=ORBIT(; δ=δ)) isa RA.ReachSolution  # TODO implement this
 
     # --------------------------------------------------------------------------
     # forced oscillator Mx'' + Kx = R, s.t. x(0) = [0, 0], x'(0) = [0, 0]
