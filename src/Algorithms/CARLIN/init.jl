09b4546e2e09668368f6c1174c8451d8c8633beb
schillic <git@christianschilling.net>
2026-02-05 23:38:25 +0100

    fix for Requires together with Revise

--- src/Algorithms/CARLIN/init.jl
+++ src/Algorithms/CARLIN/init.jl
@@ -1,5 +1,5 @@
 function __init__()
     @require DynamicPolynomials = "7c1d4256-1411-5781-91ec-d7bc3513ac07" include("init_DynamicPolynomials.jl")
     @require MultivariatePolynomials = "102ac46a-7ee4-5c85-9060-abc95bfdeaa3" include("init_MultivariatePolynomials.jl")
-    @require Symbolics = "0c5d862f-8b57-4792-8d23-62f2024744c7" nothing
+    @require Symbolics = "0c5d862f-8b57-4792-8d23-62f2024744c7" (nothing,)
 end
--- src/Algorithms/FLOWSTAR/init.jl
+++ src/Algorithms/FLOWSTAR/init.jl
@@ -1,4 +1,4 @@
 function __init__()
     @require Flowstar = "a8054ddd-9dca-4d20-8ffe-ae96ec1541f1" include("init_Flowstar.jl")
-    @require Symbolics = "0c5d862f-8b57-4792-8d23-62f2024744c7" nothing
+    @require Symbolics = "0c5d862f-8b57-4792-8d23-62f2024744c7" (nothing,)
 end
